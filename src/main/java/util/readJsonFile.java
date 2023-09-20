ec8f5b77199d13b0218a8f94a050c8df27eae021
csahoo <csahoo@processmap.com>
2023-09-20 18:24:03 +0530

    added line in readjson file

--- src/main/java/util/readJsonFile.java
+++ src/main/java/util/readJsonFile.java
@@ -5,6 +5,7 @@ public class readJsonFile {
 	public static void main(String[] args) {
 		System.out.println("test");
 		System.out.println("test_prabesh");
+		System.out.println("test_chinmaya");
 
 	}
 
